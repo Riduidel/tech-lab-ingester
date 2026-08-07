efbd20bf9804ef78def59c7573d4c02bfaa9c658
Sese-collab <serpil.coskun@zenika.com>
2026-08-07 16:57:46 +0200

    Create moduleindicator-computations-interface

---
+++ indicator-computations-interface/src/main/java/indicators/IndicatorComputer.java
@@ -0,0 +1,14 @@
+package indicators;
+
+import com.zenika.tech.lab.ingester.model.Technology;
+
+public interface IndicatorComputer {
+	String getFromRouteName();
+
+	/**
+	 * Simply says if computer can compute technology. We may need more details later ...
+	 * @param technology
+	 * @return true when indicator can compute value for technology.
+	 */
+	boolean canCompute(Technology technology);
+}
